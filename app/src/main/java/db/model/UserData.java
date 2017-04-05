1853b15210f57e44903921c724d6e3d8b7ce7bf5
sunghyunpark <sunghyun1038@naver.com>
2017-04-05 16:51:54 +0900

    realm 적용

---
+++ app/src/main/java/db/model/UserData.java
@@ -0,0 +1,62 @@
+package db.model;
+
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class UserData extends RealmObject{
+
+    @PrimaryKey
+    private int uid;
+    @Required
+    private String email;
+    @Required
+    private String birth;
+    @Required
+    private String gender;
+    @Required
+    private String created_at;
+
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+}
