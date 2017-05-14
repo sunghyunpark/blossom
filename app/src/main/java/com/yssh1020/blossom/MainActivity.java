6e5e62cbac1ebc5ca0d9aa5572cb47828f399573
sunghyunpark <sunghyun1038@naver.com>
2017-05-14 21:47:49 +0900

    bottom menu click 눌림효과 적용

--- app/src/main/java/com/yssh1020/blossom/MainActivity.java
+++ app/src/main/java/com/yssh1020/blossom/MainActivity.java
@@ -1,6 +1,7 @@
 package com.yssh1020.blossom;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -20,6 +22,7 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import db.RealmConfig;
 import db.model.UserData;
 import io.realm.Realm;
+import tab3.SelectBG_Activity;
 import view.CommonTabMenu;
 import model.User;
 import tab1.FragmentPage1;
@@ -31,7 +34,7 @@ import view.CommonTopTitle;
 
 import static tab1.FragmentPage1.mLayout;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener {
+public class MainActivity extends FragmentActivity {
 
     private SessionManager session;    // session
 
@@ -96,11 +99,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         tab3 = (ImageView)findViewById(R.id.tab_3);
         tab4 = (ImageView)findViewById(R.id.tab_4);
         tab5 = (ImageView)findViewById(R.id.tab_5);
-        tab1.setOnClickListener(this);
-        tab2.setOnClickListener(this);
-        tab3.setOnClickListener(this);
-        tab4.setOnClickListener(this);
-        tab5.setOnClickListener(this);
+        tab1.setOnTouchListener(myOnTouchListener);
+        tab2.setOnTouchListener(myOnTouchListener);
+        tab3.setOnTouchListener(myOnTouchListener);
+        tab4.setOnTouchListener(myOnTouchListener);
+        tab5.setOnTouchListener(myOnTouchListener);
 
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fm.beginTransaction();
@@ -123,102 +126,120 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         User.getInstance().setGender(user.getGender());
         User.getInstance().setCreated_at(user.getCreated_at());
         User.getInstance().setProfile_img(user.getProfile_img());
+        User.getInstance().setToken(user.getToken());
         Log.d("UserInfo", "UserUid : "+user.getUid()+"");
         Log.d("UserInfo", "UserEmail : "+user.getEmail());
         Log.d("UserInfo", "UserBirth : "+user.getBirth());
         Log.d("UserInfo", "UserGender : "+user.getGender());
         Log.d("UserInfo", "Created_at : "+user.getCreated_at());
         Log.d("UserInfo", "Profile_img : "+user.getProfile_img());
+        Log.d("UserInfo", "Push_Token : "+user.getToken());
         Toast.makeText(getApplicationContext(),User.getInstance().getUid(),Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onClick(View view) {
-        boolean is_current_page = false;
-        Fragment fragment = null;
-        /**
-         * 3번쨰 글쓰기 버튼 탭 시 하단 탭 메뉴에서 이전에 선택한 아이콘 유지하기 위해
-         */
-        if(view.getId() != R.id.tab_3){
-            tab1.setImageResource(R.mipmap.tab1_no_click_img);
-            tab2.setImageResource(R.mipmap.tab2_no_click_img);
-            tab3.setImageResource(R.mipmap.tab3_img);
-            tab4.setImageResource(R.mipmap.tab4_no_click_img);
-            tab5.setImageResource(R.mipmap.tab5_no_click_img);
-        }
 
-        Bundle bundle = new Bundle();
+    private View.OnTouchListener myOnTouchListener = new View.OnTouchListener() {
 
-        switch (view.getId()) {
-            case R.id.tab_1 :
-                tab1.setImageResource(R.mipmap.tab1_click_img);
-                CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(Color.TRANSPARENT);
-                if(current_page == 1){
-                    is_current_page = true;
-                }else{
-                    fragment = new FragmentPage1();
-                    bundle.putString("KEY_MSG", "replace");
-                    fragment.setArguments(bundle);
-                    current_page = 1;
-                    is_current_page = false;
-                    app_title_bar.setVisibility(View.VISIBLE);
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            boolean is_current_page = false;
+            Fragment fragment = null;
+            /**
+             * 3번쨰 글쓰기 버튼 탭 시 하단 탭 메뉴에서 이전에 선택한 아이콘 유지하기 위해
+             */
+            if(v.getId() != R.id.tab_3){
+                tab1.setImageResource(R.mipmap.tab1_no_click_img);
+                tab2.setImageResource(R.mipmap.tab2_no_click_img);
+                tab3.setImageResource(R.mipmap.tab3_img);
+                tab4.setImageResource(R.mipmap.tab4_no_click_img);
+                tab5.setImageResource(R.mipmap.tab5_no_click_img);
+            }
+
+            Bundle bundle = new Bundle();
+
+            Resources res = getResources();
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                v.setAlpha(0.55f);
+            }else if(event.getAction() == MotionEvent.ACTION_CANCEL){
+                v.setAlpha(1.0f);
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                v.setAlpha(1.0f);
+                switch(v.getId()){
+
+                    case R.id.tab_1 :
+                        tab1.setImageResource(R.mipmap.tab1_click_img);
+                        CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(Color.TRANSPARENT);
+                        if(current_page == 1){
+                            is_current_page = true;
+                        }else{
+                            fragment = new FragmentPage1();
+                            bundle.putString("KEY_MSG", "replace");
+                            fragment.setArguments(bundle);
+                            current_page = 1;
+                            is_current_page = false;
+                            app_title_bar.setVisibility(View.VISIBLE);
+                        }
+                        break ;
+                    case R.id.tab_2 :
+                        CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(getResources().getColor(R.color.bottomTabMenu));
+                        tab2.setImageResource(R.mipmap.tab2_click_img);
+                        if(current_page == 2){
+                            is_current_page = true;
+                        }else{
+                            fragment = new FragmentPage2();
+                            bundle.putString("KEY_MSG", "replace");
+                            fragment.setArguments(bundle);
+                            current_page = 2;
+                            is_current_page = false;
+                            app_title_bar.setVisibility(View.VISIBLE);
+                        }
+                        break ;
+                    case R.id.tab_3 :
+                        startActivity(new Intent(getApplicationContext(), Upload_Article.class));
+                        app_title_bar.setVisibility(View.VISIBLE);
+                        break ;
+                    case R.id.tab_4:
+                        tab4.setImageResource(R.mipmap.tab4_click_img);
+                        CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(getResources().getColor(R.color.bottomTabMenu));
+                        if(current_page == 4){
+                            is_current_page = true;
+                        }else{
+                            fragment = new FragmentPage4();
+                            bundle.putString("KEY_MSG", "replace");
+                            fragment.setArguments(bundle);
+                            current_page = 4;
+                            is_current_page = false;
+                            app_title_bar.setVisibility(View.GONE);
+                        }
+                        break;
+                    case R.id.tab_5:
+                        tab5.setImageResource(R.mipmap.tab5_click_img);
+                        CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(getResources().getColor(R.color.bottomTabMenu));
+                        if(current_page == 5){
+                            is_current_page = true;
+                        }else{
+                            fragment = new FragmentPage5();
+                            bundle.putString("KEY_MSG", "replace");
+                            fragment.setArguments(bundle);
+                            current_page = 5;
+                            is_current_page = false;
+                            app_title_bar.setVisibility(View.GONE);
+                        }
+                        break;
+
                 }
-                break ;
-            case R.id.tab_2 :
-                CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(getResources().getColor(R.color.bottomTabMenu));
-                tab2.setImageResource(R.mipmap.tab2_click_img);
-                if(current_page == 2){
-                    is_current_page = true;
-                }else{
-                    fragment = new FragmentPage2();
-                    bundle.putString("KEY_MSG", "replace");
-                    fragment.setArguments(bundle);
-                    current_page = 2;
-                    is_current_page = false;
-                    app_title_bar.setVisibility(View.VISIBLE);
+                // 글쓰기 버튼이 아닌경우에만...
+                if((v.getId() != R.id.tab_3) && !is_current_page){
+                    android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.replace(R.id.main_frame, fragment);
+                    fragmentTransaction.commit();
                 }
-                break ;
-            case R.id.tab_3 :
-                startActivity(new Intent(getApplicationContext(), Upload_Article.class));
-                app_title_bar.setVisibility(View.VISIBLE);
-                break ;
-            case R.id.tab_4:
-                tab4.setImageResource(R.mipmap.tab4_click_img);
-                CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(getResources().getColor(R.color.bottomTabMenu));
-                if(current_page == 4){
-                    is_current_page = true;
-                }else{
-                    fragment = new FragmentPage4();
-                    bundle.putString("KEY_MSG", "replace");
-                    fragment.setArguments(bundle);
-                    current_page = 4;
-                    is_current_page = false;
-                    app_title_bar.setVisibility(View.GONE);
-                }
-                break;
-            case R.id.tab_5:
-                tab5.setImageResource(R.mipmap.tab5_click_img);
-                CommonTabMenu.getInstance().getBottom_menu().setBackgroundColor(getResources().getColor(R.color.bottomTabMenu));
-                if(current_page == 5){
-                    is_current_page = true;
-                }else{
-                    fragment = new FragmentPage5();
-                    bundle.putString("KEY_MSG", "replace");
-                    fragment.setArguments(bundle);
-                    current_page = 5;
-                    is_current_page = false;
-                    app_title_bar.setVisibility(View.GONE);
-                }
-                break;
+            }
+            return true;
         }
-        // 글쓰기 버튼이 아닌경우에만...
-        if((view.getId() != R.id.tab_3) && !is_current_page){
-            android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-            android.support.v4.app.FragmentTransaction fragmentTransaction = fm.beginTransaction();
-            fragmentTransaction.replace(R.id.main_frame, fragment);
-            fragmentTransaction.commit();
-        }
-    }
+    };
+
     @Override
     public void onBackPressed() {
         if (FragmentPage1.mLayout.getPanelState() != SlidingUpPanelLayout.PanelState.HIDDEN){
