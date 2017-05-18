f260fd3a534e68b1fb4d5ca9032616c4d911f693
sunghyunpark <sunghyun1038@naver.com>
2017-05-18 22:06:40 +0900

    푸시 랜딩 적용중

--- app/src/main/java/push/FirebaseMessagingService.java
+++ app/src/main/java/push/FirebaseMessagingService.java
@@ -16,16 +16,60 @@ import com.google.firebase.messaging.RemoteMessage;
 import com.yssh1020.blossom.MainActivity;
 import com.yssh1020.blossom.R;
 
+import tab5.ArticleActivity;
+
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
     private static final String TAG = "FirebaseMsgService";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        sendPushNotification(remoteMessage.getData().get("message"));
+        if(remoteMessage.getData().get("flag").equals("bookmark")){
+            BookMarkSendPushNotification(remoteMessage.getData().get("message"),
+                    remoteMessage.getData().get("article_id"));
+        }else if(remoteMessage.getData().get("flag").equals("article_comment")){
+            ArticleCommentSendPushNotification(remoteMessage.getData().get("message"),
+                    remoteMessage.getData().get("article_id"));
+        }else if(remoteMessage.getData().get("flag").equals("comment_like")){
+            CommentLikeSendPushNotification(remoteMessage.getData().get("message"));
+        }else if(remoteMessage.getData().get("flag").equals("article_like")){
+            ArticleLikeSendPushNotification(remoteMessage.getData().get("message"),
+                    remoteMessage.getData().get("article_id"));
+        }
+
     }
 
-    private void sendPushNotification(String message) {
+    private void ArticleLikeSendPushNotification(String message, String article_id){
+        Resources res = getResources();
+
+        Intent intent = new Intent(this, ArticleActivity.class);
+        intent.putExtra("article_id", article_id);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.app_icon).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.app_icon) )
+                .setContentTitle(String.format(res.getString(R.string.app_name)))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri).setLights(000000255,500,2000)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakelock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        wakelock.acquire(5000);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    /**
+     * 댓글 공감
+     * @param message
+     */
+    private void CommentLikeSendPushNotification(String message){
         Resources res = getResources();
 
         Intent intent = new Intent(this, MainActivity.class);
@@ -41,7 +85,6 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri).setLights(000000255,500,2000)
                 .setContentIntent(pendingIntent);
-
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
@@ -49,6 +92,69 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         wakelock.acquire(5000);
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    /**
+     * 아티클에 댓글 남겻을 때
+     * @param message
+     * @param article_id
+     */
+    private void ArticleCommentSendPushNotification(String message, String article_id){
+        Resources res = getResources();
+
+        Intent intent = new Intent(this, ArticleActivity.class);
+        intent.putExtra("article_id", article_id);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.app_icon).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.app_icon) )
+                .setContentTitle(String.format(res.getString(R.string.app_name)))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri).setLights(000000255,500,2000)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakelock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        wakelock.acquire(5000);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    /**
+     * bookmark push
+     * @param message
+     * @param article_id
+     */
+    private void BookMarkSendPushNotification(String message, String article_id) {
+        Resources res = getResources();
+
+        Intent intent = new Intent(this, ArticleActivity.class);
+        intent.putExtra("article_id", article_id);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.app_icon).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.app_icon) )
+                .setContentTitle(String.format(res.getString(R.string.app_name)))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri).setLights(000000255,500,2000)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakelock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        wakelock.acquire(5000);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
     }
 
 }
